c933d8e53f1198b43574846243c50930a73538ac
Juan Aragon <me@juanaragonco.co>
2023-11-03 13:26:50 -0500

    (feat): Add forms validations

--- src/shared/utils/validations.ts
+++ src/shared/utils/validations.ts
@@ -4,8 +4,20 @@ const charactersOnlyEmail = /^[a-zA-ZÀ-ÿ-.-\u00f1\u00d1 @#$%^&.!¡*+=]+$/;
 const numbersOnly = /^[0-9]+$/;
 const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$/%^&*.])/;
 
-export const textValidation = () => yup.string().max(100);
-export const emailValidation = () => textValidation().email().required();
-export const passwordValidation = () => textValidation().required().min(6).matches(passwordRegex);
+type TextValidationResult<T extends boolean> =
+	T extends true ? yup.StringSchema<string> :
+	T extends false ? yup.StringSchema<string | undefined> :
+	never;
+
+export const textValidation = <T extends boolean>(required: T) : TextValidationResult<T> => {
+	let schema = yup.string().max(100);
+	if (!required) {
+		schema = schema.required();
+	}
+	return schema as TextValidationResult<T>;
+}
+
+export const emailValidation = () => textValidation(true).email();
+export const passwordValidation = () => textValidation(true).min(6).matches(passwordRegex);
 
 export { charactersOnlyEmail, numbersOnly };
