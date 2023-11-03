448ad5f941110f2cd521496bfa7f2edfc0767524
Juan Aragon <me@juanaragonco.co>
2023-11-03 08:40:40 -0500

    (feat): Add shared validations

--- src/shared/utils/validations.ts
+++ src/shared/utils/validations.ts
@@ -1,4 +1,11 @@
+import * as yup from "yup";
+
 const charactersOnlyEmail = /^[a-zA-ZÀ-ÿ-.-\u00f1\u00d1 @#$%^&.!¡*+=]+$/;
 const numbersOnly = /^[0-9]+$/;
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$/%^&*.])/;
+
+export const textValidation = () => yup.string().max(100);
+export const emailValidation = () => textValidation().email().required();
+export const passwordValidation = () => textValidation().required().min(6).matches(passwordRegex);
 
 export { charactersOnlyEmail, numbersOnly };
