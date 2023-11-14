bd1f943257a9d6cc8bba9f203925110b1471b5e0
Edison Alba <ejalba02@misena.edu.co>
2023-11-14 09:51:28 -0500

    change in layout for login, register and recover password

--- src/infrastructure/components/auth/login/index.tsx
+++ src/infrastructure/components/auth/login/index.tsx
@@ -1,37 +1,26 @@
 import React from "react";
-import Image from "next/image";
 
 import { LoginForm } from "@infrastructure/containers";
-import LogoImage from "/public/sentria_white.png";
+import LayoutComponent from "@infrastructure/components/auth/layout";
 
-import {
-	LoginContainer,
-	LoginContainerLeft,
-	LoginContainerRight,
-	ContentForm,
-	SecondTitleCustom,
-	TitleCustom,
-	ContentHeaderLogo,
-} from "./styled";
+import { ContentForm, SecondTitleCustom, TitleCustom } from "./styled";
 
-const LoginComponent: React.FC = () => (
-	<LoginContainer>
-		<ContentHeaderLogo>
-			<Image src={LogoImage} width={150} height={80} alt="Logo" priority />
-		</ContentHeaderLogo>
-		<LoginContainerLeft>
-			<ContentForm>
-				<TitleCustom center>¡Bienvenido a Sentria!</TitleCustom>
-				<SecondTitleCustom center>
-					Inicia sesión para mantenerte al tanto de tus reportes
-				</SecondTitleCustom>
-				<LoginForm onSubmit={() => {}} />
-			</ContentForm>
-		</LoginContainerLeft>
-		<LoginContainerRight>
-			<Image src={LogoImage} width={272} height={159} alt="Logo" priority />
-		</LoginContainerRight>
-	</LoginContainer>
+interface ILoginComponentProps {
+	login: () => void;
+}
+
+const LoginComponent: React.FC<ILoginComponentProps> = ({
+	login
+}: ILoginComponentProps) => (
+	<LayoutComponent>
+		<ContentForm>
+			<TitleCustom center>¡Bienvenido a Sentria!</TitleCustom>
+			<SecondTitleCustom center>
+				Inicia sesión para mantenerte al tanto de tus reportes
+			</SecondTitleCustom>
+			<LoginForm onSubmit={() => login()} />
+		</ContentForm>
+	</LayoutComponent>
 );
 
 export default LoginComponent;
