6beafa78d133afc6df5edd5fecf983f0a6bf3d43
Juan Aragon <me@juanaragonco.co>
2023-11-21 10:39:48 -0500

    (feat): Add firebase config

--- src/shared/utils/firebase.ts
+++ src/shared/utils/firebase.ts
@@ -1,18 +1,31 @@
 // Import the functions you need from the SDKs you need
+import { VAPID_KEY } from "@shared/constants";
 import { initializeApp } from "firebase/app";
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAa5kv01Pt2OB8nYbzErFLOVv04uJw4VR8",
-  authDomain: "sentria-3af49.firebaseapp.com",
-  projectId: "sentria-3af49",
-  storageBucket: "sentria-3af49.appspot.com",
-  messagingSenderId: "975597926589",
-  appId: "1:975597926589:web:4850079f55371b375adfd4",
+	apiKey: "AIzaSyAa5kv01Pt2OB8nYbzErFLOVv04uJw4VR8",
+	authDomain: "sentria-3af49.firebaseapp.com",
+	projectId: "sentria-3af49",
+	storageBucket: "sentria-3af49.appspot.com",
+	messagingSenderId: "975597926589",
+	appId: "1:975597926589:web:4850079f55371b375adfd4",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app;
+
+const messaging = getMessaging(app);
+
+onMessage(messaging, (payload) => {
+	/* eslint-disable no-console -- Console.log for testing API keys */
+	console.log("foreground message", { payload });
+});
+
+getToken(messaging, { vapidKey: VAPID_KEY }).then((token) =>
+	console.log({ token })
+);
