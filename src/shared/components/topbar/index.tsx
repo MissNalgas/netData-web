50ae9a56cc329114498bbd797bb8f1ea936f5d4a
Nayelly Rueda <angienayelly01@gmail.com>
2023-11-23 08:59:13 -0500

    Add icon account in topbar

--- src/shared/components/topbar/index.tsx
+++ src/shared/components/topbar/index.tsx
@@ -1,6 +1,7 @@
 import { useAuth } from "@infrastructure/containers/auth";
 import Image from "next/image";
 import alarm from "/public/img/alarm_icon.png"
+import Icon from "../icons";
 
 export default function Topbar() {
 	const auth = useAuth();
@@ -8,7 +9,9 @@ export default function Topbar() {
 	return (
 		<div className="w-full h-20 flex justify-between items-center px-2 bg-white">
 			<div className="flex gap-2 items-center">
-				<div className="w-12 h-12 rounded-full bg-gray-100"/>
+				<div className="w-12 h-12 rounded-full bg-gray-100 items-center flex justify-center">
+                    <Icon icon="account" size={22}/>
+                </div>
 				<span>
 					Hola, <span className="text-primary font-semibold">{auth.user?.firstname}</span>
 				</span>
