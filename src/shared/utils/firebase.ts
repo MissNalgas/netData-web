100307421607821f23855ab42c39c09ce570b0db
Juan Aragon <me@juanaragonco.co>
2023-11-24 11:55:24 -0500

    (feat): Add loading component

--- src/shared/utils/firebase.ts
+++ src/shared/utils/firebase.ts
@@ -11,7 +11,7 @@ const firebaseConfig = {
 	appId: "1:975597926589:web:4850079f55371b375adfd4",
 };
 
-let app : FirebaseApp | undefined;
+let app: FirebaseApp | undefined;
 
 if (typeof window !== "undefined") {
 	app = initializeApp(firebaseConfig);
@@ -26,7 +26,6 @@ if (typeof window !== "undefined") {
 	getToken(messaging, { vapidKey: VAPID_KEY }).then((token) =>
 		console.log({ token })
 	);
-
 }
 
 export default app;
