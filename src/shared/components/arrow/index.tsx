e29f55102beb21fb9b9b2f3bd9a36dc579dc3ee9
Juan Aragon <me@juanaragonco.co>
2023-11-28 14:13:42 -0500

    (feat): Add tooltip library
    Add library that handles tooltip positioning

--- src/shared/components/arrow/index.tsx
+++ src/shared/components/arrow/index.tsx
@@ -1,12 +1,34 @@
+import { FloatingArrow, arrow, flip, offset, useFloating, useHover, useInteractions } from "@floating-ui/react";
 import Icon from "../icons";
+import { useRef, useState } from "react";
 
 export default function Arrow({ action, nameIcon }: { action: () => void, nameIcon: string }) {
-    return(
-        <div className="bg-shadow20 rounded-full ml-1 h-7" onClick={action}>
-            <Icon
-                icon={nameIcon}
-                size={25}
-            />
-        </div>
-    )
+
+	const [isOpen, setIsOpen] = useState(false);
+	const arrowRef = useRef(null);
+	const { refs, floatingStyles, context } = useFloating({
+		open: isOpen,
+		onOpenChange: setIsOpen,
+		middleware: [offset(12), flip(), arrow({element: arrowRef})],
+		placement: "top",
+	});
+	const hover = useHover(context);
+	const {getReferenceProps, getFloatingProps } = useInteractions([hover]);
+
+	return (
+		<>
+			<div ref={refs.setReference} className="bg-shadow20 rounded-full ml-1 h-7" onClick={action} {...getReferenceProps()}>
+				<Icon
+					icon={nameIcon}
+					size={25}
+				/>
+			</div>
+			<div className={`bg-gray-800 text-white p-1 rounded transition ${isOpen ? "opacty-100" : "opacity-0"}` }style={floatingStyles} ref={refs.setFloating} {...getFloatingProps()}>
+				<span>
+					Ver más
+				</span>
+				<FloatingArrow context={context} ref={arrowRef}/>
+			</div>
+		</>
+	);
 }
