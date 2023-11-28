e2ee75df654f0e0314ec119eb9abee1406139e69
Edison Alba <ejalba02@misena.edu.co>
2023-11-28 18:25:35 -0500

    add front profile

---
+++ src/infrastructure/components/profile/index.tsx
@@ -0,0 +1,123 @@
+import { useRouter } from "next/navigation";
+
+import ContainerBackground from "@shared/components/containerBackground";
+
+import { CaptionOne, SubtitleLink } from "@shared/components/labels/styled";
+
+import {
+	PrimaryButton,
+	SecondaryButton,
+} from "@shared/components/buttons/styled";
+import { useAuth } from "@infrastructure/containers/auth";
+import Icon from "@shared/components/icons";
+import { useTheme } from "styled-components";
+import Logo from "/public/img/logo-sentria.png";
+
+import {
+	ContentBody,
+	ContentHeader,
+	ContentImage,
+	ContentLogo,
+	ImageProfile,
+} from "./styled";
+
+export default function ProfileComponent() {
+	const auth = useAuth();
+	const theme = useTheme();
+	const router = useRouter();
+	return (
+		<main className="flex space-between mx-5 py-8 h-screen mb-32">
+			<ContainerBackground className="grow justify-center p-10 h-min	">
+				<div className="p-10">
+					<ContentHeader>
+						<ContentLogo>
+							<ContentImage>
+								<ImageProfile src={Logo} alt="logo" />
+							</ContentImage>
+						</ContentLogo>
+
+						<div className="w-12 h-12 rounded-full bg-gray-100 items-center flex justify-center">
+							<Icon icon="account" size={22} />
+						</div>
+					</ContentHeader>
+
+					<div className="flex flex-col justify-center items-center gap-y-2 p-10">
+						<div>
+							<SubtitleLink $weight={600}>
+								Hola,
+								<span className="text-primary font-semibold">
+									{auth.user?.firstname}
+								</span>
+							</SubtitleLink>
+						</div>
+						<div>
+							<span className="text-primary font-semibold">
+								<Icon
+									icon="office-building"
+									size={24}
+									color={theme.colors.orange50}
+								/>
+							</span>
+							<CaptionOne className="text-primary" $weight={600}>
+								Redeban
+							</CaptionOne>
+						</div>
+					</div>
+
+					<ContentBody>
+						<PrimaryButton
+							disabled={false}
+							type="submit"
+							width={30}
+						>
+							<div className="flex flex-row gap-5">
+								<Icon
+									icon="info-circle"
+									size={24}
+									color="white"
+								/>
+								Guía de ayuda
+							</div>
+						</PrimaryButton>
+						<PrimaryButton
+							disabled={false}
+							width={30}
+							onClick={() => router.push("/notifications")}
+						>
+							<div className="flex flex-row gap-5">
+								<Icon icon="Bell" size={24} color="white" />
+								Notificaciones
+							</div>
+						</PrimaryButton>
+						<PrimaryButton
+							disabled={false}
+							type="submit"
+							width={30}
+						>
+							<div className="flex flex-row gap-5">
+								<Icon
+									icon="Paper-Plane"
+									size={24}
+									color="white"
+								/>
+								Reportar problemas de la app
+							</div>
+						</PrimaryButton>
+					</ContentBody>
+					<div className="flex justify-center my-8">
+						<SecondaryButton disabled={false} width={30}>
+							<div className="flex flex-row gap-5">
+								<Icon
+									icon="account"
+									size={24}
+									color={theme.colors.orange50}
+								/>
+								Eliminar cuenta
+							</div>
+						</SecondaryButton>
+					</div>
+				</div>
+			</ContainerBackground>
+		</main>
+	);
+}
--- src/shared/components/topbar/index.tsx
+++ src/shared/components/topbar/index.tsx
@@ -1,29 +1,39 @@
 import { useAuth } from "@infrastructure/containers/auth";
+import { useRouter } from "next/navigation";
+
 import Image from "next/image";
-import alarm from "/public/img/alarm_icon.png"
+import alarm from "/public/img/alarm_icon.png";
 import Icon from "../icons";
 
 export default function Topbar() {
 	const auth = useAuth();
+	const router = useRouter();
 
 	return (
 		<div className="w-full h-20 flex justify-between items-center px-2 bg-white">
-			<div className="flex gap-2 items-center">
+			<div
+				className="flex gap-2 items-center"
+				onClick={() => router.push("/profile")}
+			>
 				<div className="w-12 h-12 rounded-full bg-gray-100 items-center flex justify-center">
-                    <Icon icon="account" size={22}/>
-                </div>
-				<span>
-					Hola,
-					<span className="text-primary font-semibold">
-						{auth.user?.firstname}
+					<Icon icon="account" size={22} />
+				</div>
+				<div className="flex flex-col">
+					<span>
+						Hola,
+						<span className="text-primary font-semibold">
+							{auth.user?.firstname}
+						</span>
 					</span>
-				</span>
+					<span className="flex flex-row items-center gap-1">
+						Última actualización: <b>10:00 am</b>
+						<Icon icon="Reload" size={22} />
+					</span>
+				</div>
 			</div>
-			<span>
-				Última actualización: <b>10:00 am</b>
-			</span>
+
 			<div className="w-12 h-12 bg-[#d4c0dd] rounded-full grid place-content-center">
-				<Image src={alarm} alt="Alarm" width={32} height={0}/>
+				<Image src={alarm} alt="Alarm" width={32} height={0} />
 			</div>
 		</div>
 	);
