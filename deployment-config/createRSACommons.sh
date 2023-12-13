1122c534cee7e12a80adb3f41cb03a31039b4333
Edison Alba <ejalba02@misena.edu.co>
2023-12-13 09:42:57 -0500

    change in folder

--- deployment-config/createRSACommons.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo -e $BACKEND_COMMONS_KEY >  ~/.ssh/BackendCommonsKey
-cat <<EOT >> ~/.ssh/config
-Host github.com
-  IdentityFile ~/.ssh/BackendCommonsKey
-EOT
--- deployment-config/environment_setter.js
+++ deployment-config/environment_setter.js
@@ -1,23 +1,5 @@
 const fs = require("fs").promises;
-const ENV_VARS = [
-	"DATABASE_USER",
-	"DATABASE_HOST",
-	"DATABASE_NAME",
-	"DATABASE_PORT",
-	"DATABASE_PASSWORD",
-	"SERVER_APP_PORT",
-	"NODE_ENV",
-	"REDIS_URL",
-	"SECRET_PASSWORD",
-	"REDIS_HOSTNAME",
-	"REDIS_PORT",
-	"API_PREFIX",
-	"APP_PORT",
-	"API_URL",
-	"API_URL_IMAGES",
-	"VAPID_KEY",
-	"NEXT_PUBLIC_API_URL",
-];
+const ENV_VARS = ["API_URL", "API_URL_IMAGES", "VAPID_KEY"];
 
 let contentVariables = "";
 for (const varName of ENV_VARS) {
@@ -30,5 +12,4 @@ for (const varName of ENV_VARS) {
 
 (async () => {
 	await fs.writeFile("./.env", contentVariables, "utf8");
-	await fs.writeFile("./env.production", contentVariables, "utf8");
 })();
