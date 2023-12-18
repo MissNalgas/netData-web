e615328c412d04306764b8be76a4a0af1414282d
Edison Alba <ejalba02@misena.edu.co>
2023-12-18 10:45:14 -0500

    add new styles

--- src/shared/components/chat/index.tsx
+++ src/shared/components/chat/index.tsx
@@ -1,17 +1,38 @@
 import { JSX } from "react";
 import TextInput from "../textInput";
-import { TitleTree } from "../labels/styled";
+import { CaptionOne, Body } from "../labels/styled";
 import Icon from "../icons";
 
 export default function Chat(): JSX.Element {
 	return (
 		<div>
-			<div>
-				<Icon icon="user" size={20} />
-				<TitleTree>ID 1234456</TitleTree>
+			<div className="w-full flex justify-between py-3">
+				<Icon icon="Cancel" size={20} />
+				<Body $weight={700}>ID 1234456</Body>
 			</div>
-			¿Tienes dudas? manda una comentario Xelco
-			<TextInput placeholder="Escribe tu mensaje" name={""} />
+			<div className="w-64 text-right">
+				<CaptionOne>
+					¿Tienes dudas? manda una comentario Xelco
+				</CaptionOne>
+			</div>
+
+			<TextInput
+				placeholder="Escribe tu mensaje"
+				name={""}
+				iconright="Arrow-Button"
+				className="
+								disabled:text-gray-400
+								border borde-gray-100
+								rounded-lg
+								h-10
+								focus:outline-gray20
+								z-10
+								px-2
+								w-full
+								mt-1
+								pr-12
+						"
+			/>
 		</div>
 	);
 }
