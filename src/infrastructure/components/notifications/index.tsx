0399b872636cbd35b54b596feba39959ba297fba
Edison Alba <ejalba02@misena.edu.co>
2023-12-21 10:55:04 -0500

    add filter notifications for date

--- src/infrastructure/components/notifications/index.tsx
+++ src/infrastructure/components/notifications/index.tsx
@@ -17,6 +17,7 @@ import { format } from "date-fns";
 import { es, enUS } from "date-fns/locale";
 import { toast } from "react-toastify";
 import { useTranslation } from "react-i18next";
+import { getFormattedDate } from "@shared/utils";
 
 export default function NotificationsComponent() {
 	const dispatch = useAppDispatch();
@@ -29,6 +30,57 @@ export default function NotificationsComponent() {
 		}
 	}, [dispatch, notificationsData.error, t]);
 	const date = new Date();
+
+	const listNotifications = [...notificationsData.data]
+		.sort(
+			(a, b) =>
+				new Date(b.created_at).getTime() -
+				new Date(a.created_at).getTime()
+		)
+		.reduce((prev: any, curr: any) => {
+			let index = getFormattedDate(
+				new Date(curr.created_at),
+				i18n.language
+			);
+			prev[index] !== undefined
+				? prev[index].push(curr)
+				: (prev[index] = [curr]);
+			return prev;
+		}, {});
+	const renderNotifications = () => {
+		if (notificationsData.data.length > 0) {
+			return Object.keys(listNotifications).map((date: any) => (
+				<div key={date}>
+					<h1>{date}</h1>
+					{listNotifications[date].map(
+						(notification: NotificationItem) => (
+							<InformationCard
+								key={notification.id}
+								imageLeft={
+									(notification.risk === "Low" && Tree) ||
+									(notification.risk === "Urgent" && Fire) ||
+									(notification.risk === "High" && Alarm) ||
+									(notification.risk === "Medium" && Clock) ||
+									Fire
+								}
+								textLeft={notification.message_body}
+								textRight={notification.ticket_id}
+								showIconLeft={false}
+							/>
+						)
+					)}
+				</div>
+			));
+		} else {
+			return notificationsData?.data?.length === 0 ? (
+				<Overline $weight={theme.fontWeight.bold}>
+					{t("you_do_not_have_any_notifications")}
+				</Overline>
+			) : (
+				<LoaderComponent />
+			);
+		}
+	};
 	return (
 		<div className="flex space-between mx-5 py-8 h-screen mb-32 overflow-auto">
 			<ContainerBackground className="grow justify-center mr-8">
@@ -47,35 +99,7 @@ export default function NotificationsComponent() {
 					</CaptionTwo>
 				</div>
 
-				{notificationsData?.data?.length > 0 ? (
-					notificationsData?.data?.map(
-						(notification: NotificationItem) => (
-							<div key={notification.id}>
-								<InformationCard
-									imageLeft={
-										(notification.risk === "Low" && Tree) ||
-										(notification.risk === "Urgent" &&
-											Fire) ||
-										(notification.risk === "High" &&
-											Alarm) ||
-										(notification.risk === "Medium" &&
-											Clock) ||
-										Fire
-									}
-									textLeft={notification.message_body}
-									textRight={notification.ticket_id}
-									showIconLeft={false}
-								/>
-							</div>
-						)
-					)
-				) : notificationsData?.data?.length === 0 ? (
-					<Overline $weight={theme.fontWeight.bold}>
-						{t("you_do_not_have_any_notifications")}
-					</Overline>
-				) : (
-					<LoaderComponent />
-				)}
+				{renderNotifications()}
 			</ContainerBackground>
 
 			<ContainerBackground className="flex items-center flex-col">
