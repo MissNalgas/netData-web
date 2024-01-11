4d758ee95a007aaae389a9db94db072552378a15
Edison Alba <ejalba02@misena.edu.co>
2024-01-11 18:14:54 -0500

    change in code

--- src/infrastructure/api/http/axios.ts
+++ src/infrastructure/api/http/axios.ts
@@ -2,7 +2,7 @@ import { store } from "@infrastructure/store";
 import { resetState } from "@infrastructure/store/user/actions";
 import { API_URL } from "@shared/constants";
 import axios from "axios";
-// import { toast } from "react-toastify";
+import { toast } from "react-toastify";
 
 export async function createAxios() {
 	const instance = axios.create({
@@ -31,14 +31,23 @@ export async function createAxiosApp() {
 		(error) => {
 			if (error.response) {
 				const validateToken = error.response.data.name;
+				localStorage.setItem("isExpired", validateToken);
 
 				if (
-					validateToken === "TokenExpiredError" ||
-					(validateToken === "JsonWebTokenError" && !errorToastShown)
+					(localStorage.getItem("isExpired") ===
+						"TokenExpiredError" &&
+						validateToken === "TokenExpiredError" &&
+						!errorToastShown) ||
+					(localStorage.getItem("isExpired") ===
+						"JsonWebTokenError" &&
+						validateToken === "JsonWebTokenError" &&
+						!errorToastShown)
 				) {
-					// toast.error("Tu sesión ha expirado");
+					toast.error("Tu sesión ha expirado");
 					errorToastShown = true;
 					localStorage.removeItem("tokenApp");
+					localStorage.removeItem("isExpired");
+
 					store.dispatch(resetState());
 				}
 			}
