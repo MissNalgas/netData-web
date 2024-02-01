5df65adfe9bf9043d063fa48849c9563d88db15a
Nayelly Rueda <angienayelly01@gmail.com>
2024-02-01 12:40:52 -0500

    delete code don't use

--- src/infrastructure/components/dashboard/index.tsx
+++ src/infrastructure/components/dashboard/index.tsx
@@ -19,8 +19,7 @@ import {
 } from "@infrastructure/store/dashboard/actions";
 import { useAppDispatch } from "@hooks/use-dispatch";
 import Topbar from "@shared/components/topbar";
-import { Day, Ticket, TicketPriority } from "@domain/models";
-import { TicketStatus } from "@shared/constants/statusList";
+import { Day, TicketPriority } from "@domain/models";
 import { dashboardDataInitial } from "@infrastructure/store/dashboard/types";
 import LoaderComponent from "@shared/components/loader";
 import { JoyrideToast } from "@shared/components/tooltip/list/JoyrideToast";
@@ -33,7 +32,6 @@ export default function Dashboard() {
 	);
 	const [day, setDay] = useState<"today" | "yesterday">("today");
 	const [dashboardDay, setDashboardDay] = useState<Day>(dashboardDataInitial);
-	const [_priorityTickets, setPriorityTickets] = useState<number[]>([]);
 	const joyrideRef = useRef();
 
 	useEffect(() => {
@@ -85,31 +83,11 @@ export default function Dashboard() {
 
 	useEffect(() => {
 		if (dashboard) {
-			let ticketsPriority;
 			if (day === "yesterday") {
 				setDashboardDay(dashboard?.yesterday);
-				ticketsPriority =
-					dashboard?.yesterday?.ticketsForPriorityByDepartment
-						?.filter((ticket: Ticket) =>
-							[
-								TicketStatus?._open,
-								TicketStatus?._pending,
-							].includes(ticket?.status)
-						)
-						.map((ticket: Ticket) => ticket?.priority);
 			} else {
 				setDashboardDay(dashboard.today);
-				ticketsPriority =
-					dashboard.today?.ticketsForPriorityByDepartment
-						?.filter((ticket: Ticket) =>
-							[
-								TicketStatus?._open,
-								TicketStatus?._pending,
-							].includes(ticket?.status)
-						)
-						.map((ticket: Ticket) => ticket?.priority);
 			}
-			setPriorityTickets(ticketsPriority ?? []);
 		}
 	}, [dashboard, day]);
 
