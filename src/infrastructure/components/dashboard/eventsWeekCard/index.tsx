ba6b9238464cc69133d4ba4cfea885abf1acc544
Nayelly Rueda <angienayelly01@gmail.com>
2024-02-02 09:25:43 -0500

    delete progress bar component and create iframe with char of bars in cybersecurity events of the week

--- src/infrastructure/components/dashboard/eventsWeekCard/index.tsx
+++ src/infrastructure/components/dashboard/eventsWeekCard/index.tsx
@@ -1,19 +1,17 @@
 import Arrow from "@shared/components/arrow";
 import ContainerBackground from "@shared/components/containerBackground";
-import { BodyTwo, CaptionTwo, Overline } from "@shared/components/labels/styled";
+import { BodyTwo, CaptionTwo } from "@shared/components/labels/styled";
 import { ContainerFlex } from "../styled";
 import theme from "@theme/index";
 import CircleStatus from "@shared/components/circleStatus";
 import { useTranslation } from "react-i18next";
-import { IGraphicWeek } from "@domain/models";
 import { useRouter } from "next/navigation";
-import ProgressBar from "@shared/components/progressBar";
+import { IGraphicWeek } from "@domain/models";
 
 export default function EventsWeekCard(props: IGraphicWeek) {
     const { open, closed } = props;
     const { t } = useTranslation("dashboard");
     const router = useRouter();
-    const maxIncidents = 500;
 
     return (
         <ContainerBackground className="my-4" id="step-5">
@@ -21,26 +19,23 @@ export default function EventsWeekCard(props: IGraphicWeek) {
                 <CaptionTwo $weight={theme.fontWeight.bold}>{t("events_of_the_week")}</CaptionTwo>
                 <Arrow action={() => router.push("/events-cibersecurity")} nameIcon="right-arrow"/>
             </ContainerFlex>
-            <ContainerFlex>
-                <ContainerFlex>
-                    <ProgressBar incidents={open ?? 0} maxIncidents={maxIncidents} bgColor={theme.colors.red40}/>
-                    <Overline $weight={theme.fontWeight.bold}>{open ?? 0}</Overline>
-                </ContainerFlex>
-                <ContainerFlex className="items-center">
-                    <CircleStatus internalColor="red40" externalColor="red20"/>
-                    <BodyTwo $color={theme.colors.red40} $weight={theme.fontWeight.bold}>{t("open")}</BodyTwo>
-                </ContainerFlex>
-            </ContainerFlex>
-            <ContainerFlex className="mt-2">
-                <ContainerFlex>
-                    <ProgressBar incidents={closed ?? 0} maxIncidents={maxIncidents} bgColor={theme.colors.green40}/>
-                    <Overline $weight={theme.fontWeight.bold}>{closed ?? 0}</Overline>
-                </ContainerFlex>
-                <ContainerFlex className="items-center">
-                    <CircleStatus internalColor="green40" externalColor="green20"/>
-                    <BodyTwo $color={theme.colors.green40} $weight={theme.fontWeight.bold}>{t("closed")}</BodyTwo>
-                </ContainerFlex>
-            </ContainerFlex>
+            <div className="flex space-between bg-blue-200">
+                <iframe
+                    src={`/chart/progressbars?height=500&events-open=${open}&events-closed=${closed}`}
+                    className="h-[80px] flex bg-red-200 pt-2"
+                    title="solutionbars"
+                />
+                <div className="flex flex-col justify-between my-3">
+                    <ContainerFlex className="items-center">
+                        <CircleStatus internalColor="red40" externalColor="red20"/>
+                        <BodyTwo $color={theme.colors.red40} $weight={theme.fontWeight.bold}>{t("open")}</BodyTwo>
+                    </ContainerFlex>
+                    <ContainerFlex className="items-center">
+                        <CircleStatus internalColor="green40" externalColor="green20"/>
+                        <BodyTwo $color={theme.colors.green40} $weight={theme.fontWeight.bold}>{t("closed")}</BodyTwo>
+                    </ContainerFlex>
+                </div>
+            </div>
         </ContainerBackground>
     )
 }
--- src/shared/components/circleStatus/index.tsx
+++ src/shared/components/circleStatus/index.tsx
@@ -7,8 +7,8 @@ export default function CircleStatus (props: ICircleStatusProps) {
     const { internalColor, externalColor } = props;
 
     return (
-        <div className={`${externalColor} w-4 h-4 rounded-full flex items-center justify-center mr-2`}>
-            <div className={`${internalColor} rounded-full w-2 h-2`}/>
+        <div className={`bg-${externalColor} w-4 h-4 rounded-full flex items-center justify-center mr-2`}>
+            <div className={`bg-${internalColor} rounded-full w-2 h-2`}/>
         </div>
     )
 };
